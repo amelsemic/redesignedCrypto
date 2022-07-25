d0cf718bc9707ff1e6f8d99b5b19a47725a12f66
amelsemic <semicamel@hotmail.com>
2022-07-25 12:54:01 +0200

    UI fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,13 @@ const Header = (props) => {
   return (
     <div className={classes.header}>
       <p>
-        <span style={{ fontWeight: "bold" }}>CRYPTOCURRENCY // </span>
-        <span>value tracker</span>
+        <span className={classes.left}>CRYPTOCURRENCY </span>
+        <span className={classes.right}>// value tracker</span>
       </p>
       <input
         type="text"
-        placeholder="search over 200 coins"
+        placeholder="search over 200 coins..."
         onChange={props.onInputChange}
-        
       />
     </div>
   );
